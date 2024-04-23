47e480151a3e3728d945a0a8780fb3f6458558f5
jtdcapstone <163462709+jtdcapstone@users.noreply.github.com>
2024-04-23 16:52:53 -0400

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -11,7 +11,7 @@ st.write("Welcome Dr. Givens!")
 
 st.link_button("Click for chatbot", "https://orange-happiness-jjr775ggv66vf5vxp-8501.app.github.dev/")
 
-st.link_button("Click for notepad", "https://note-jtdcapstone.streamlit.app/")
+st.link_button("Click for notepad", "https://note-jtdcapstone.streamlit.app/?embed=true")
 
 
 st.link_button("Click for reminders", "https://super-space-fiesta-4jqvv7rr5x6pcq5gg-8501.app.github.dev/")
